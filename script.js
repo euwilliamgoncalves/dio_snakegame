796d083a45963b3ec690477da5dd24143ce648eb
William Goncalves <euwilliamgoncalves@gmail.com>
2020-06-04 19:22:18 -0300

    Adicionando comandos pelo teclado e aumentando a resolução do jogo

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let canvas = document.getElementById("snake")
 let context = canvas.getContext("2d")
-let box = 32
+let box = 16
 let snake = []
 
 snake[0] = {
@@ -12,7 +12,7 @@ let direction = "right"
 
 function criarBG() {
     context.fillStyle = "lightgreen"
-    context.fillRect(0, 0, 16 * box, 16 * box)
+    context.fillRect(0, 0, 32 * box, 32 * box)
 }
 
 function criarCobrinha() {
@@ -20,6 +20,15 @@ function criarCobrinha() {
         context.fillStyle = "#131313"
         context.fillRect(snake[i].x, snake[i].y, box, box)
     }
+}
+
+document.addEventListener("keydown", update);
+
+function update (event) {
+    if (event.keyCode == 37 && direction != "right") direction = "left"
+    if (event.keyCode == 39 && direction != "left") direction = "right"
+    if (event.keyCode == 38 && direction != "down") direction = "up"
+    if (event.keyCode == 40 && direction != "up") direction = "down"
 }
 
 function iniciarJogo() {
@@ -45,4 +54,3 @@ function iniciarJogo() {
 }
 
 let jogo = setInterval(iniciarJogo, 100)
-
