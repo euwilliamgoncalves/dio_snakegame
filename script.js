83b73eb1adb8e768983d556d8aff1da461693b03
William Goncalves <euwilliamgoncalves@gmail.com>
2020-06-05 10:44:11 -0300

    Alterando estilos e adicionando teclado virtual

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 let canvas = document.getElementById("snake")
 let context = canvas.getContext("2d")
-let box = 8
+let box = 10
 let snake = []
 
 snake[0] = {
-    x: 8 * box,
-    y: 8 * box
+    x: 10 * box,
+    y: 10 * box
 }
 
 let direction = "right"
 
 let food = {
-    x: Math.floor(Math.random() * 49 + 1) * box,
-    y: Math.floor(Math.random() * 49 + 1) * box
+    x: Math.floor(Math.random() * 39 + 1) * box,
+    y: Math.floor(Math.random() * 39 + 1) * box
 }
 
 function criarBG() {
-    context.fillStyle = "#cccccc"
-    context.fillRect(0, 0, 50 * box, 50 * box)
+    context.fillStyle = "#131313"
+    context.fillRect(0, 0, 40 * box, 40 * box)
 }
 
 function criarCobrinha() {
     for (i=0; i < snake.length; i++) {
-        context.fillStyle = "#131313"
+        context.fillStyle = "#efefef"
         context.fillRect(snake[i].x, snake[i].y, box, box)
     }
 }
 
 function drawFood(){
-    context.fillStyle = "#ff4b4b"
+    context.fillStyle = "red"
     context.fillRect(food.x, food.y, box, box)
 }
 
@@ -41,11 +41,30 @@ function update (event) {
     if (event.keyCode == 40 && direction != "up") direction = "down"
 }
 
+const upTouch = document.querySelector(".tecla[id=uptouch]")
+const dwTouch = document.querySelector(".tecla[id=dwtouch]")
+const lfTouch = document.querySelector(".tecla[id=lftouch]")
+const rgTouch = document.querySelector(".tecla[id=rgtouch]")
+
+upTouch.addEventListener("click", () => {if (direction != "down") direction = "up"})
+dwTouch.addEventListener("click", () => {if (direction != "up") direction = "down"})
+lfTouch.addEventListener("click", () => {if (direction != "right") direction = "left"})
+rgTouch.addEventListener("click", () => {if (direction != "left") direction = "right"})
+
 function iniciarJogo() {
-    if (snake[0].x > 49 * box && direction == "right") snake[0].x = 0
-    if (snake[0].x < 0 * box && direction == "left") snake[0].x = 49 * box
-    if (snake[0].y > 49 * box && direction == "down") snake[0].y = 0
-    if (snake[0].y < 0 * box && direction == "up") snake[0].y = 49 * box
+
+    for (i = 1; i < snake.length; i++){
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(jogo)
+            alert("Game Over! :(")
+        }
+    }
+
+    if (snake[0].x > 39 * box && direction == "right") snake[0].x = 0
+    if (snake[0].x < 0 * box && direction == "left") snake[0].x = 39 * box
+    if (snake[0].y > 39 * box && direction == "down") snake[0].y = 0
+    if (snake[0].y < 0 * box && direction == "up") snake[0].y = 39 * box
+
     criarBG()
     criarCobrinha()
     drawFood()
@@ -53,17 +72,17 @@ function iniciarJogo() {
     let snakeX = snake[0].x
     let snakeY = snake[0].y
 
+    if (snakeX != food.x || snakeY != food.y) {
+            snake.pop()
+    }else{
+        food.x = Math.floor(Math.random() * 39 + 1) * box,
+        food.y = Math.floor(Math.random() * 39 + 1) * box
+    }
+    
     if (direction == "right") snakeX += box
     if (direction == "left") snakeX -= box
     if (direction == "up") snakeY -= box
     if (direction == "down") snakeY += box
-
-    if (snakeX != food.x || snakeY != food.y) {
-        snake.pop()
-    }else{
-        food.x = Math.floor(Math.random() * 49 + 1) * box,
-        food.y = Math.floor(Math.random() * 49 + 1) * box
-    }    
 
     let newHead = {
         x: snakeX,
@@ -73,6 +92,4 @@ function iniciarJogo() {
     snake.unshift(newHead)
 }
 
-let tempo = 100
-
-let jogo = setInterval(iniciarJogo, tempo)
+let jogo = setInterval(iniciarJogo, 200)
